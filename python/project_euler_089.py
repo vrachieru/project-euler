5e8b1348b98ec30e7dbc01b9ce93574dfa9edfcb
Victor Rachieru <victor.rachieru@xwiki.com>
2015-02-20 16:59:46 +0200

    [misc] Fix filenames and code cleanup

--- python/project_euler_089.py
+++ python/project_euler_089.py
@@ -3,11 +3,11 @@ import os
 
 def shorten(roman):
     replacements = [
-        ("VIIII", "IX"), 
-        ("IIII" , "IV"), 
-        ("LXXXX", "XC"), 
+        ("VIIII", "IX"),
+        ("IIII" , "IV"),
+        ("LXXXX", "XC"),
         ("XXXX" , "XL"),
-        ("DCCCC", "CM"), 
+        ("DCCCC", "CM"),
         ("CCCC" , "CD"),
     ]
     for old, new in replacements:
@@ -17,7 +17,7 @@ def shorten(roman):
 original = 0
 improved = 0
 
-for line in open(os.path.join(os.path.dirname(__file__), '../resources/p89-roman.txt')):
+for line in open(os.path.join(os.path.dirname(__file__), '../resources/p089_roman.txt')):
     line = line.strip()
     original += len(line)
     improved += len(shorten(line))
